87cc750ce9afbdcef26b5246db60c15f79a00c13
joe <jawoheer@gmail.com>
2024-08-08 15:07:15 +0500

    toggle button

---
+++ src/hooks/useTelegram.js
@@ -0,0 +1,23 @@
+const tg = window.Telegram.WebApp;
+
+export function useTelegram(){
+    
+    const onClose = () => {
+        tg.close()
+    }
+
+    const onToggle = () => {
+        if(tg.MainButton.isVisible){
+            tg.MainButton.hide();
+        } else{
+            tg.MainButton.show();
+        }
+    }
+
+    return {
+        tg,
+        onClose,
+        onToggle,
+        user: tg.initDataUnsafe?.user,
+    }
+}
